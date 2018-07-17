585dc3ade2e6ab5daa3f7bca21b643d5bbdb24ca
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-16 21:58:10 -0500

    Added logic so function works with negative values as well

--- JS/app.js
+++ JS/app.js
@@ -9,15 +9,17 @@ const arr = [
 	[0, 0, 1, 2, 4, 0]
 ];
 
+
 const hourGlassSum = (array) => {
-	let sum = 0;
+	let sum;
 	for (let i = 0; i <= 3; i ++) {
 		for (let j = 0; j <= 3; j ++) {
 			let top    = array[i][j] + array[i][j+1] + array[i][j+2],
 				middle = array[i+1][j+1],
 				bottom = array[i+2][j] + array[i+2][j+1] + array[i+2][j+2],
 				total  = top + middle + bottom;
-			if (total > sum) {
+				console.log(total);
+			if (total > sum || sum === undefined) {
 				sum = total;
 			}	
 		};
@@ -27,3 +29,21 @@ const hourGlassSum = (array) => {
 };
 
 console.log(hourGlassSum(arr));
+
+
+
+
+
+
+
+
+const negArr = [
+	[10, -1, -1, 0, 0, 0],
+	[0, -1, 0, 0, 0, 0],
+	[-1, -1, -1, 0, 0, 0],
+	[0, 0, -2, -4, -4, 0],
+	[0, 0, 0, -2, 0, 0],
+	[0, 0, -1, -2, -4, 0]
+];
+
+console.log(hourGlassSum(negArr));
